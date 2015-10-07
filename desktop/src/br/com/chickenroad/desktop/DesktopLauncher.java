2acda2b99b2bd34909932966c1435a697a773a38
barroslipe <barroslipe@gmail.com>
2015-10-07 01:11:43 -0300

    Commit inicial para adicionar o projeto no repositório

---
+++ desktop/src/br/com/chickenroad/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package br.com.chickenroad.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import br.com.chickenroad.ChickenRoadGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ChickenRoadGame(), config);
+	}
+}
