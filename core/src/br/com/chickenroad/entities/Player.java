6afdbca1f918c389180efb26ed03ea34271cb797
barroslipe <barroslipe@gmail.com>
2015-10-14 19:24:52 -0300

    Retirado bug da aplicação ao rodar no android. A câmera e o objeto player não estavam sincronizados. O player sumia da tela nas extremidadesquando o acelerômetro era utilizado.

--- core/src/br/com/chickenroad/entities/Player.java
+++ core/src/br/com/chickenroad/entities/Player.java
@@ -1,5 +1,8 @@
 package br.com.chickenroad.entities;
 
+import br.com.chickenroad.ChickenRoadGame;
+import br.com.chickenroad.screens.Play;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.Texture;
@@ -8,108 +11,121 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 
-import br.com.chickenroad.ChickenRoadGame;
-import br.com.chickenroad.screens.Play;
-
-//executada a cada frame
 public class Player extends Sprite implements InputProcessor {
+
 	private Vector2 velocity = new Vector2();
 	float speed = 60*2;
-	float gravity = 60*0.8f;
 	private int pontoX = 0; //ponto de click/toque na tela
 	private int pontoY = 0;
-	public boolean movendoX1 = false;//x positivo - diz ppara que lado o avatar esta movendo
-	public boolean movendoX2 = false;//x negativo 
-	public boolean movendoY1 = false;//y positivo 
-	public boolean movendoY2 = false;//y negativo
-	
+	private boolean movendoX1 = false;//x positivo - diz ppara que lado o avatar esta movendo
+	private boolean movendoX2 = false;//x negativo 
+	private boolean movendoY1 = false;//y positivo 
+	private boolean movendoY2 = false;//y negativo
+
+	private int widthTiledMap, heightTileMap;
+
 	private ChickenRoadGame chickenRoadGame;
-	
-	public Player(String sprite, ChickenRoadGame aChickenRoadGame) {
+
+	public Player(String sprite, ChickenRoadGame aChickenRoadGame, int aWidthTiledMap, int aHeightTiledMap) {
 		super(new Texture(sprite));
-		
+
 		this.chickenRoadGame = aChickenRoadGame;
+		this.widthTiledMap = aWidthTiledMap;
+		this.heightTileMap = aHeightTiledMap;
+
 	}
-	
+
 	public void update(float delta) {
+
+
 		if(velocity.x > speed)
-		    velocity.x = speed;
+			velocity.x = speed;
 		if(velocity.x < -speed)
 			velocity.x = -speed;
 		if(velocity.y > speed)
 			velocity.y = speed;
 		if(velocity.y < -speed)
 			velocity.y = -speed;
-		
+
 		//usando ao acelerometro 
 
 		//esquerda
-		if(Gdx.input.getPitch() > 30) {
+		if(Gdx.input.getPitch() > 15) {
 			velocity.x = -speed/2;
 			movendoX1 = false;
 			movendoX2 = false;
 			movendoY1 = false;
 			movendoY2 = false;
 		}
-		
+
 		//direita
-		else if(Gdx.input.getPitch() < -30) {
+		else if(Gdx.input.getPitch() < -15) {
 			velocity.x = speed/2;
 			movendoX1 = false;
 			movendoX2 = false;
 			movendoY1 = false;
 			movendoY2 = false;
 		}
-		
+
 		else if(!movendoX1 && !movendoX2 &&
 				!movendoY1 && !movendoY2) {
 			velocity.x = 0;
 		}
-		
+
 		//cima
-		if(Gdx.input.getRoll() > 30) {
+		if(Gdx.input.getRoll() > 15) {
 			velocity.y = -speed/2;
 			movendoX1 = false;
 			movendoX2 = false;
 			movendoY1 = false;
 			movendoY2 = false;
 		}
-		
+
 		//baixo
-		else if(Gdx.input.getRoll() < -30) {
+		else if(Gdx.input.getRoll() < -15) {
 			velocity.y = speed/2;
 			movendoX1 = false;
 			movendoX2 = false;
 			movendoY1 = false;
 			movendoY2 = false;
 		}
-		
+
 		else if(!movendoX1 && !movendoX2 &&
 				!movendoY1 && !movendoY2)
 			velocity.y = 0;
-			
-		//if(Gdx.input.getAzimuth()) { }
-		
-		//
-		this.setX(this.getX()+velocity.x*delta);
-		this.setY(this.getY()+velocity.y*delta);
-		
+
+		float newPositionX = this.getX()+velocity.x*delta;
+		float newPositionY = this.getY()+velocity.y*delta;
+
+		if(newPositionX > widthTiledMap-40)
+			newPositionX = widthTiledMap-40;
+		if(newPositionX < 0)
+			newPositionX = 0;
+		if(newPositionY > heightTileMap-64)
+			newPositionY = heightTileMap-64;
+		if(newPositionY <0)
+			newPositionY = 0;
+
+		this.setX(newPositionX);
+		this.setY(newPositionY);
+
+
 		//regras de parada de movimento
 		if(movendoX1 && getX()+this.getWidth()/2 >= pontoX) {
 			movendoX1 = false;
 			velocity.x = 0;
 		}
-		
+
 		if(movendoY1 && getY()+this.getHeight()/2 >= pontoY) {
 			movendoY1 = false;
 			velocity.y = 0;
 		}
-		
+
 		if(movendoX2 && getX()+this.getWidth()/2 <= pontoX) {
 			movendoX2 = false;
 			velocity.x = 0;
 		}
-		
+
 		if(movendoY2 && getY()+this.getHeight()/2 <= pontoY) {
 			movendoY2 = false;
 			velocity.y = 0;
@@ -120,7 +136,7 @@ public class Player extends Sprite implements InputProcessor {
 		//desenha o personagem
 		update(Gdx.graphics.getDeltaTime());
 		super.draw(chickenRoadGame.getSpriteBatch());
-		
+
 	}
 	@Override
 	public boolean keyDown(int keycode) {
@@ -147,12 +163,13 @@ public class Player extends Sprite implements InputProcessor {
 	}
 
 	@Override
-	//evento para liberação de toque na tela - quando solta a tela
+	//evento para liberaÃ§Ã£o de toque na tela - quando solta a tela
 	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-		Vector3 ponto = Play.orthographicCamera.unproject(new Vector3(screenX, screenY, 0)); //usa o screenZ=0 (2D)
+
+		Vector3 ponto = Play.orthographicCamera.unproject(new Vector3(screenX, screenY, 0));
 		pontoX = (int)ponto.x;
 		pontoY = (int)ponto.y;
-		
+
 		if(pontoX > getX()){
 			velocity.x = speed;
 			movendoX1 = true;
@@ -189,5 +206,5 @@ public class Player extends Sprite implements InputProcessor {
 		// TODO Auto-generated method stub
 		return false;
 	}
-	
+
 }
--- core/src/br/com/chickenroad/screens/Play.java
+++ core/src/br/com/chickenroad/screens/Play.java
@@ -1,5 +1,8 @@
 package br.com.chickenroad.screens;
 
+import br.com.chickenroad.ChickenRoadGame;
+import br.com.chickenroad.entities.Player;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
@@ -8,22 +11,19 @@ import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
-import br.com.chickenroad.ChickenRoadGame;
-import br.com.chickenroad.entities.Player;
-
 public class Play implements Screen {
-	
+
 	private String url_map;
-	
+
 	private ChickenRoadGame chickenRoadGame;
-	
+
 	private TiledMap tiledMap; //guarda o mapa
 	private int widthTiledMap, heightTiledMap;
-	
+
 	private OrthogonalTiledMapRenderer orthogonalTiledMapRenderer; //guarda o renderizador do mapa
 	public static OrthographicCamera orthographicCamera;//cria camera
 	private Player player;
-	
+
 	public Play(String aMap, ChickenRoadGame aChickenRoadGame) {
 		this.url_map = aMap;
 		this.chickenRoadGame = aChickenRoadGame;
@@ -36,45 +36,43 @@ public class Play implements Screen {
 		orthogonalTiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
 		widthTiledMap = tiledMap.getProperties().get("width", Integer.class)*tiledMap.getProperties().get("tilewidth", Integer.class);
 		heightTiledMap = tiledMap.getProperties().get("height", Integer.class)*tiledMap.getProperties().get("tileheight", Integer.class);
-		
+
 		orthographicCamera = new OrthographicCamera();
-		
+
 		//TODO qual o tamanho da camera?
 		orthographicCamera.setToOrtho(false, 640,480);
 
 		//TODO parametrizar para iniciar com outro personagem
-		player = new Player("player/chk1.png", chickenRoadGame);
-		
+		player = new Player("player/chk1.png", chickenRoadGame, widthTiledMap, heightTiledMap);
+
 		//TODO parametrizar, o ponto de origem do jogador pode mudar de acordo com o mapa a ser apresentado
 		player.setX(0);
 		player.setY(0);
 
-		//permite que os eventos de entrada da aplicação vá para o Player tratar
 		Gdx.input.setInputProcessor(player);
-		
+
 	}
 
 	@Override
 	public void render(float delta) {
-		
+
 		Gdx.gl.glClearColor(0, 0, 0, 1); //cor preta
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		
+
 		//renderiza camera e imagens
 		orthogonalTiledMapRenderer.setView(orthographicCamera);
 		orthogonalTiledMapRenderer.render();
 
 		//faz a camera seguir o player
 		orthographicCamera.position.set(player.getX(), player.getY(), 0);
-		
-		
+
+
 		chickenRoadGame.getSpriteBatch().begin();
 		player.draw(chickenRoadGame.getSpriteBatch());
 		chickenRoadGame.getSpriteBatch().end();
 
-		
 		positionCamera();
-		
+
 		orthographicCamera.update();
 		chickenRoadGame.getSpriteBatch().setProjectionMatrix(orthographicCamera.combined);
 	}
@@ -85,39 +83,35 @@ public class Play implements Screen {
 		if(orthographicCamera.position.x < 320) {
 			orthographicCamera.position.x = 320;
 		}
-		
+
 		if(orthographicCamera.position.y < 240) {
 			orthographicCamera.position.y = 240;
 		}
-	
+
 		if(orthographicCamera.position.x > widthTiledMap-320) {
 			orthographicCamera.position.x = widthTiledMap-320;
 		}
-		
+
 		if(orthographicCamera.position.y > heightTiledMap-240) {
 			orthographicCamera.position.y = heightTiledMap-240;
 		}
-		
+
 	}
 
 	@Override
 	public void resize(int width, int height) {
-		//chamado quando o usuario alterar o tamanho da janela
-		orthographicCamera.viewportWidth = width;
-		orthographicCamera.viewportHeight = height;
-		orthographicCamera.update();
 
 	}
 
 	@Override
 	public void pause() {
-		
+
 
 	}
 
 	@Override
 	public void resume() {
-		
+
 
 	}
 
